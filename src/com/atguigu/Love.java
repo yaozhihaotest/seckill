2dff249a3c9f7d2fae23e4ed8d1dd1adba9571ec
yzh <1078102940@qq.com>
2020-08-08 19:45:51 +0800

    test

--- src/com/atguigu/Love.java
+++ src/com/atguigu/Love.java
@@ -4,5 +4,6 @@ public class Love {
     public static void main(String[] args) {
         System.out.println("love yaozhihao");
         System.out.println("love dev");
+        System.out.println("test");
     }
 }
